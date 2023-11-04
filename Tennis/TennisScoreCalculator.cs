5b8865a0c654c39558a9950c92d47655dc773f4f
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-11-04 16:26:08 +0100

    Duplication removed for second player's score

--- Tennis/TennisScoreCalculator.cs
+++ Tennis/TennisScoreCalculator.cs
@@ -4,21 +4,27 @@ public class TennisScoreCalculator
 {
     public string Score(int player1Points, int player2Points)
     {
-        if (player1Points == 1)
+        if (player1Points > 0 || player2Points > 0)
         {
-            return "Fifteen-Love";
+            string result = string.Empty;
+
+            if (player1Points == 1)
+            {
+                result = "Fifteen";
+            }
+            else if (player1Points == 2)
+            {
+                result = "Thirty";
+            }
+            else if (player1Points == 3)
+            {
+                result = "Forty";
+            }
+
+            return $"{result}-Love";
+
         }
 
-        if (player1Points == 2)
-        {
-            return "Thirty-Love";
-        }
-
-        if (player1Points == 3)
-        {
-            return "Forty-Love";
-        }
-        
         return "Love-All";
     }
 }
