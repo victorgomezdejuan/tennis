a9c007f605fe241cee1ebc3242d9a670f7923993
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-11-04 17:09:00 +0100

    Added test cases for simple win scores

--- Tennis.Tests/TennisScoreCalculatorAdvantageOrWinScoreTests.cs
+++ Tennis.Tests/TennisScoreCalculatorAdvantageOrWinScoreTests.cs
@@ -22,5 +22,21 @@ namespace Tennis.Tests
             var score = calculator.Score(3, 4);
             Assert.Equal("Advantage player2", score);
         }
+
+        [Fact]
+        public void WinForPlayer1()
+        {
+            var calculator = new TennisScoreCalculator();
+            var score = calculator.Score(5, 3);
+            Assert.Equal("Win for player1", score);
+        }
+
+        [Fact]
+        public void WinForPlayer2()
+        {
+            var calculator = new TennisScoreCalculator();
+            var score = calculator.Score(3, 5);
+            Assert.Equal("Win for player2", score);
+        }
     }
 }
