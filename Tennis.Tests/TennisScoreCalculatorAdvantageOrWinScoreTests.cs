a870d693003614a6f962df2dd460ef7830a5fdc3
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-11-04 17:13:42 +0100

    Added more advanced test cases for advantage and win scores

--- Tennis.Tests/TennisScoreCalculatorAdvantageOrWinScoreTests.cs
+++ Tennis.Tests/TennisScoreCalculatorAdvantageOrWinScoreTests.cs
@@ -24,6 +24,22 @@ namespace Tennis.Tests
         }
 
         [Fact]
+        public void AdvantagePlayer1AfterDeuce()
+        {
+            var calculator = new TennisScoreCalculator();
+            var score = calculator.Score(5, 4);
+            Assert.Equal("Advantage player1", score);
+        }
+
+        [Fact]
+        public void AdvantagePlayer2AfterDeuce()
+        {
+            var calculator = new TennisScoreCalculator();
+            var score = calculator.Score(4, 5);
+            Assert.Equal("Advantage player2", score);
+        }
+
+        [Fact]
         public void WinForPlayer1()
         {
             var calculator = new TennisScoreCalculator();
@@ -36,6 +52,22 @@ namespace Tennis.Tests
         {
             var calculator = new TennisScoreCalculator();
             var score = calculator.Score(3, 5);
+            Assert.Equal("Win for player2", score);
+        }
+
+        [Fact]
+        public void WinForPlayer1AfterDeuce()
+        {
+            var calculator = new TennisScoreCalculator();
+            var score = calculator.Score(6, 4);
+            Assert.Equal("Win for player1", score);
+        }
+
+        [Fact]
+        public void WinForPlayer2AfterDeuce()
+        {
+            var calculator = new TennisScoreCalculator();
+            var score = calculator.Score(4, 6);
             Assert.Equal("Win for player2", score);
         }
     }
