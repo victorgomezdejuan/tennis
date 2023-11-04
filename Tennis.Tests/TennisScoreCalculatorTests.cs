96149891f53f073b24b098bcaf819053743c3472
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-11-04 17:04:26 +0100

    Added "Deuce" case, renamed the other draw scores and added some functionality that GitHub Copilot suggested and seems very promissing

--- Tennis.Tests/TennisScoreCalculatorTests.cs
+++ Tennis.Tests/TennisScoreCalculatorTests.cs
@@ -3,14 +3,6 @@ namespace Tennis.Tests;
 public class TennisScoreCalculatorTests
 {
     [Fact]
-    public void LoveAll()
-    {
-        var calculator = new TennisScoreCalculator();
-        var score = calculator.Score(0, 0);
-        Assert.Equal("Love-Love", score);
-    }
-
-    [Fact]
     public void FifteenLove()
     {
         var calculator = new TennisScoreCalculator();
@@ -59,11 +51,19 @@ public class TennisScoreCalculatorTests
     }
 
     [Fact]
+    public void LoveAll()
+    {
+        var calculator = new TennisScoreCalculator();
+        var score = calculator.Score(0, 0);
+        Assert.Equal("Love-All", score);
+    }
+
+    [Fact]
     public void FifteenAll()
     {
         var calculator = new TennisScoreCalculator();
         var score = calculator.Score(1, 1);
-        Assert.Equal("Fifteen-Fifteen", score);
+        Assert.Equal("Fifteen-All", score);
     }
 
     [Fact]
@@ -71,6 +71,14 @@ public class TennisScoreCalculatorTests
     {
         var calculator = new TennisScoreCalculator();
         var score = calculator.Score(2, 2);
-        Assert.Equal("Thirty-Thirty", score);
+        Assert.Equal("Thirty-All", score);
+    }
+
+    [Fact]
+    public void Deuce()
+    {
+        var calculator = new TennisScoreCalculator();
+        var score = calculator.Score(3, 3);
+        Assert.Equal("Deuce", score);
     }
 }
--- Tennis/TennisScoreCalculator.cs
+++ Tennis/TennisScoreCalculator.cs
@@ -4,6 +4,68 @@ public class TennisScoreCalculator
 {
     public string Score(int player1Points, int player2Points)
     {
+
+        if (player1Points == player2Points)
+        {
+            return EqualScore(player1Points);
+        }
+        else if (player1Points >= 4 || player2Points >= 4)
+        {
+            return AdvantageOrWin(player1Points, player2Points);
+        }
+        else
+        {
+            return SimpleScore(player1Points, player2Points);
+        }
+    }
+
+    private static string EqualScore(int points)
+    {
+        if (points == 0)
+        {
+            return "Love-All";
+        }
+        else if (points == 1)
+        {
+            return "Fifteen-All";
+        }
+        else if (points == 2)
+        {
+            return "Thirty-All";
+        }
+        else if (points == 3)
+        {
+            return "Deuce";
+        }
+        else
+        {
+            throw new ArgumentException("Invalid points value");
+        }
+    }
+
+    private static string AdvantageOrWin(int player1Points, int player2Points)
+    {
+        var minusResult = player1Points - player2Points;
+        if (minusResult == 1)
+        {
+            return "Advantage player1";
+        }
+        else if (minusResult == -1)
+        {
+            return "Advantage player2";
+        }
+        else if (minusResult >= 2)
+        {
+            return "Win for player1";
+        }
+        else
+        {
+            return "Win for player2";
+        }
+    }
+
+    private static string SimpleScore(int player1Points, int player2Points)
+    {
         string result = string.Empty;
         result += SinglePlayerSimpleScore(player1Points);
         result += "-";
