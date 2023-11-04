2299c714eb0f02d4e8543bccea29e30d3c322d56
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-11-04 16:37:34 +0100

    Duplication removed for single player score calculation

--- Tennis/TennisScoreCalculator.cs
+++ Tennis/TennisScoreCalculator.cs
@@ -5,44 +5,34 @@ public class TennisScoreCalculator
     public string Score(int player1Points, int player2Points)
     {
         string result = string.Empty;
-
-        if (player1Points == 0)
-        {
-            result = "Love";
-        }
-        else if (player1Points == 1)
-        {
-            result = "Fifteen";
-        }
-        else if (player1Points == 2)
-        {
-            result = "Thirty";
-        }
-        else if (player1Points == 3)
-        {
-            result = "Forty";
-        }
-
-
+        result += SinglePlayerSimpleScore(player1Points);
         result += "-";
-
-        if (player2Points == 0)
-        {
-            result += "Love";
-        }
-        else if (player2Points == 1)
-        {
-            result += "Fifteen";
-        }
-        else if (player2Points == 2)
-        {
-            result += "Thirty";
-        }
-        else if (player2Points == 3)
-        {
-            result += "Forty";
-        }
+        result += SinglePlayerSimpleScore(player2Points);
 
         return result;
+    }
+
+    private static string SinglePlayerSimpleScore(int points)
+    {
+        if (points == 0)
+        {
+            return "Love";
+        }
+        else if (points == 1)
+        {
+            return "Fifteen";
+        }
+        else if (points == 2)
+        {
+            return "Thirty";
+        }
+        else if (points == 3)
+        {
+            return "Forty";
+        }
+        else
+        {
+            throw new ArgumentException("Invalid points value");
+        }
     }
 }
